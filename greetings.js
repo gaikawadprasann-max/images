f9f86b1bdaa8f708f679d0d4365b97f48e337595
gaikawadprasann-max <gaikawadprasann@gmail.com>
2026-02-26 22:51:56 +0530

    Add greetings and motivations arrays in greetings.js

---
+++ greetings.js
@@ -0,0 +1,11 @@
+// greeting.js
+const MOTIVATIONS = [
+  "Show up even when it sucks",
+  "Nobody is coming to save you",
+  // ... rest of your quotes
+];
+
+const GREETINGS_POOL = [
+  "Hey Prasann, look who finally showed up.",
+  // ... rest of your greetings
+];
